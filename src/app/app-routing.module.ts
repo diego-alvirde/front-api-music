86bb8a7f6b68f8bfcf9d4a1c0f7d16fa56b54709
Diego Alvirde Martinez <diego.alvirde@gmail.com>
2019-08-08 15:20:10 -0500

    Creación del componente album

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NuevoComponent } from './nuevo/nuevo.component';
+import { AlbumComponent } from './album/album.component';
 
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'nuevo', component: NuevoComponent },
+  { path: 'album', component: AlbumComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'home'}
 ];
 
