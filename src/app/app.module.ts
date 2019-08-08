105ea9b23f8f74139add29808c9b61f619eece40
Diego Alvirde Martinez <diego.alvirde@gmail.com>
2019-08-08 14:47:35 -0500

    Se agregaron los componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NuevoComponent } from './nuevo/nuevo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NuevoComponent
   ],
   imports: [
     BrowserModule
